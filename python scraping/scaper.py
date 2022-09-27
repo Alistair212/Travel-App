91541a075c895b5aeed82d871f5db5aa64f1164b
Alistair <horw0017@flinders.edu.au>
2022-09-27 14:00:18 +0930

    Update scaper.py

--- python scraping/scaper.py
+++ python scraping/scaper.py
@@ -4,17 +4,11 @@ import time
 from bs4 import BeautifulSoup
 import re
 
-
-# CLEANR = re.compile('<.*?>') 
-
-# def cleanhtml(raw_html):
-#   cleantext = re.sub(CLEANR, '', raw_html)
-#   return cleantext
-
-
-
 # Set the URL you want to webscrape from
 url = 'https://www.health.gov.au/health-alerts/covid-19/international-travel'
+
+# Enter the paragraph number you want to scrape
+paragraph_num = 1
 
 # Connect to the URL
 response = requests.get(url)
@@ -23,5 +17,5 @@ print(response)
 
 # # Parse HTML and save to BeautifulSoup object¶
 soup = BeautifulSoup(response.text, "html.parser")
-one_a_tag = soup.findAll('p')[1]
+one_a_tag = soup.findAll('p')[paragraph_num]
 print((one_a_tag))
